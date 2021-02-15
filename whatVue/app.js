2af209f4a296e0e0ed612e9e907232043080bc71
Grymyl <russian.bogd@gmail.com>
2021-02-15 09:42:30 +0300

    Examined Loops with v-for

--- whatVue/app.js
+++ whatVue/app.js
@@ -7,6 +7,12 @@ Vue.createApp({
     }*/
     data: () => ({
         myHtml: '<h1>Vue 3 App</h1>',
-        title: 'Я есть Грут'
+        title: 'Я есть Грут',
+        person: {
+            firstName: 'Ruslan',
+            lastName: 'Bogdanov',
+            age: 23
+        },
+        items: [1, 2, 3, 4, 5, 6]
     })
 }).mount('#app')
