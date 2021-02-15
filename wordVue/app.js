f0bb663397bb329df7fcb196adefa3415ed50969
Grymyl <russian.bogd@gmail.com>
2021-02-15 13:15:38 +0300

    Learned Virtual DOM and render

--- wordVue/app.js
+++ wordVue/app.js
@@ -1,15 +1,32 @@
+const h = Vue.h
 const app = Vue.createApp({
     data() {
         return {
             title: 'Это свойство из template'
         }
     },
-    template: `
+    methods: {
+        changeTitle() {
+            this.title = 'Изменили!'
+        }
+    },
+    /*template: `
         <div class = "card center">
             <h1>{{ title }}</h1>
             <button class="btn" @click="title = 'Изменил!'">Изменить</button>
         </div>
-    `
+    `*/
+    render() {
+        return h('div', {
+            class: 'card center'
+        }, [
+            h('h1', {}, this.title),
+            h('buttom', {
+                class:'btn',
+                onClick: this.changeTitle
+            }, 'Изменить')
+        ])
+    },
 })
 
 app.mount('#app')
